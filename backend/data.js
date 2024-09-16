d5ac86a6ca33bb7abb1487c7f427cc5c1c88dc5f
solenwa <solen.wanono@gmail.com>
2024-09-16 15:36:26 +0200

    Product model, router + seeding

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,7 @@
 const data = {
   products: [
     {
-      _id: '1',
+      //_id: '1',
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -14,7 +14,7 @@ const data = {
       description: 'high quality shirt',
     },
     {
-      _id: '2',
+      //_id: '2',
       name: 'Adidas Slim Shirt',
       slug: 'adidas-slim-shirt',
       category: 'Shirts',
@@ -27,7 +27,7 @@ const data = {
       description: 'pretty shirt',
     },
     {
-      _id: '3',
+      //_id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -40,7 +40,7 @@ const data = {
       description: 'high quality pants',
     },
     {
-      _id: '4',
+      //_id: '4',
       name: 'Adidas Slim Pant',
       slug: 'adidas-slim-pant',
       category: 'Pants',
