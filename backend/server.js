fc7e71c8d58f463245ef504edb7b5c1e393c98ae
solenwa <solen.wanono@gmail.com>
2024-09-24 11:38:48 +0200

    Revert "Preparing for deployment"
    This reverts commit 61ca79ac79bdeb93d48369502e9fa0b13e27a2b7.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoute.js';
-import path from 'path';
 
 dotenv.config();
 
@@ -31,12 +30,6 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
-
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
